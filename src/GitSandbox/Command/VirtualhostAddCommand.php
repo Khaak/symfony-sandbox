bdbd4c81dddd0913b8200aa67fd987bd01901aec
Khaak <khaak127@gmail.com>
2015-05-25 18:02:33 +0500

    final

--- src/GitSandbox/Command/VirtualhostAddCommand.php
+++ src/GitSandbox/Command/VirtualhostAddCommand.php
@@ -9,7 +9,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
-
 class VirtualhostAddCommand extends Command {
 	protected function configure() {
 		$this
@@ -30,10 +29,10 @@ class VirtualhostAddCommand extends Command {
 				InputArgument::OPTINAL,
 				''
 			)->addArgument(
-				'encoding',
-				InputArgument::OPTIONAL,
-				''
-			)			
+			'encoding',
+			InputArgument::OPTIONAL,
+			''
+		)
 		;
 	}
 	protected function interact(InputInterface $input, OutputInterface $output) {
@@ -63,8 +62,8 @@ class VirtualhostAddCommand extends Command {
 			);
 			$input->setArgument('project', $project);
 		}
-		$tmp_type=$input->getArgument('type');
-		if (!$input->getArgument('user') && $tmp_type=="sandbox") {
+		$tmp_type = $input->getArgument('type');
+		if (!$input->getArgument('user') && $tmp_type == "sandbox") {
 			$user = $this->getHelper('dialog')->askAndValidate(
 				$output,
 				'Please enter a user name:',
@@ -83,7 +82,7 @@ class VirtualhostAddCommand extends Command {
 				'Please choose encoding (UTF-8|windows-1251):',
 				function ($encoding) {
 					if (empty($encoding)) {
-						$encoding="UTF-8";
+						$encoding = "UTF-8";
 					}
 					return $encoding;
 				}
@@ -96,13 +95,13 @@ class VirtualhostAddCommand extends Command {
 		if (posix_geteuid() !== 0) {
 			throw new \Exception('This script should be run as root.');
 		}
-		$type=$input->getArgument('type');
+		$type = $input->getArgument('type');
 		$projectname = $input->getArgument('project');
 		$fs = new Filesystem();
 		if (!$fs->exists($project_path_full)) {
 			throw new \Exception('Project ' . $projectname . ' not exists');
 		}
-		if($type=="sandbox"){
+		if ($type == "sandbox") {
 			$user = $input->getArgument('user');
 			$handle = @fopen("/etc/passwd", "r");
 			if ($handle) {
@@ -118,54 +117,54 @@ class VirtualhostAddCommand extends Command {
 			}
 		}
 		$encoding = $input->getArgument('encoding');
-		if($encoding=="UTF-8"){
-			$character="DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci";
-			$charset="utf-8";
-			$phpoverload=" ";
-			$phpinternal=" ";
-		}elseif($encoding=="windows-1251"){
-			$character="DEFAULT CHARACTER SET cp1251 COLLATE cp1251_general_ci";
-			$charset="windows-1251";
-			$phpoverload="php_admin_value mbstring.func_overload 0";
-			$phpinternal="php_admin_value mbstring.internal_encoding latin";
+		if ($encoding == "UTF-8") {
+			$character = "DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci";
+			$charset = "utf-8";
+			$phpoverload = " ";
+			$phpinternal = " ";
+		} elseif ($encoding == "windows-1251") {
+			$character = "DEFAULT CHARACTER SET cp1251 COLLATE cp1251_general_ci";
+			$charset = "windows-1251";
+			$phpoverload = "php_admin_value mbstring.func_overload 0";
+			$phpinternal = "php_admin_value mbstring.internal_encoding latin";
 		}
-		$vhost_name=$projectname.".".$settings["DOMAIN"];
-		$vhost_root="/home/" . $settings["PROJECT_USER"] . "/projects/" . $projectname . "/httpdocs";
-		if($type=="sandbox"){
-			$vhost_name=$user.".".$vhost_name;
-			$vhost_root="/home/" . $user . "/www/" . $projectname . "/httpdocs";
+		$vhost_name = $projectname . "." . $settings["DOMAIN"];
+		$vhost_root = "/home/" . $settings["PROJECT_USER"] . "/projects/" . $projectname . "/httpdocs";
+		if ($type == "sandbox") {
+			$vhost_name = $user . "." . $vhost_name;
+			$vhost_root = "/home/" . $user . "/www/" . $projectname . "/httpdocs";
 		}
-		$phpsessionsavepath='/tmp/php_sessions/'.$vhost_name
-		$phpuploadtmpdir='/tmp/php_upload/'.$vhost_name
+		$phpsessionsavepath = '/tmp/php_sessions/' . $vhost_name;
+		$phpuploadtmpdir = '/tmp/php_upload/' . $vhost_name;
 
 		$fs->mkdir($phpsessionsavepath, 0770);
 		$fs->mkdir($phpuploadtmpdir, 0770);
-		if($user){
-			$tmp_user=$user;
-		}else{
-			$tmp_user=$settings["PROJECT_USER"];
+		if ($user) {
+			$tmp_user = $user;
+		} else {
+			$tmp_user = $settings["PROJECT_USER"];
 		}
 		$fs->chown($phpsessionsavepath, $tmp_user);
 		$fs->chgrp($phpuploadtmpdir, $settings["PROJECT_GROUP"]);
 		$fs->chown($phpsessionsavepath, $tmp_user);
 		$fs->chgrp($phpuploadtmpdir, $settings["PROJECT_GROUP"]);
-		$nginxtemplatefile=$settings["TEMPLATES_DIR"]."/".$type."/nginx/site_template.conf";
-		$httpdtemplatefile=$settings["TEMPLATES_DIR"]."/".$type."/httpd/site_template.conf";
-		$nginxconffile=$settings["NGINX_CONF_DIR"]."sandbox/site_avaliable/".$vhost_name.".conf";
-		$httpdconffile=$settings["HTTPD_CONF_DIR"]."/sandbox/conf/".$vhost_name.".conf";
-		$nginxtemplate=file_get_contents($nginxtemplatefile);
+		$nginxtemplatefile = $settings["TEMPLATES_DIR"] . "/" . $type . "/nginx/site_template.conf";
+		$httpdtemplatefile = $settings["TEMPLATES_DIR"] . "/" . $type . "/httpd/site_template.conf";
+		$nginxconffile = $settings["NGINX_CONF_DIR"] . "sandbox/site_avaliable/" . $vhost_name . ".conf";
+		$httpdconffile = $settings["HTTPD_CONF_DIR"] . "/sandbox/conf/" . $vhost_name . ".conf";
+		$nginxtemplate = file_get_contents($nginxtemplatefile);
 		$tags = array("#SERVER_NAME#", "#SERVER_DIR#", "#SERVER_ENCODING#");
 		$set = array($vhost_name, $vhost_root, $charset);
 		$nginxtemplate = str_replace($tags, $set, $nginxtemplate);
 		$fs->dumpFile($nginxconffile, $nginxtemplate);
 		unset($nginxtemplate);
-		$fs->symlink($nginxconffile, $settings["NGINX_CONF_DIR"]."sandbox/site_enabled/".$vhost_name.".conf");
-		$httpdtemplate=file_get_contents($httpdtemplatefile);
+		$fs->symlink($nginxconffile, $settings["NGINX_CONF_DIR"] . "sandbox/site_enabled/" . $vhost_name . ".conf");
+		$httpdtemplate = file_get_contents($httpdtemplatefile);
 		$tags = array("#SERVER_NAME#", "#SERVER_DIR#", "#PHP_OVERLOAD#", "#PHP_INTERNAL#", "#PHP_SESSIONS#", "#PHP_UPLOAD#");
-		$set = array($vhost_name, $vhost_root, $phpoverload, $phpinternal, $phpsessionsavepath,$phpuploadtmpdir);
-		if($type=="sandbox"){
-			$tags[]="#USER_GROUP#";
-			$set[]=$user." ".$settings["PROJECT_GROUP"];
+		$set = array($vhost_name, $vhost_root, $phpoverload, $phpinternal, $phpsessionsavepath, $phpuploadtmpdir);
+		if ($type == "sandbox") {
+			$tags[] = "#USER_GROUP#";
+			$set[] = $user . " " . $settings["PROJECT_GROUP"];
 		}
 		$httpdtemplate = str_replace($tags, $set, $httpdtemplate);
 		$fs->dumpFile($httpdconffile, $httpdtemplate);
